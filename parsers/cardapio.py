8adc7c794634ca7a3adea77cf9ff16da76754613
Manoel Cândido <candidomanoel78@gmail.com>
2024-08-02 22:30:40 -0300

    enviando atividade parse cardapio

--- parsers/cardapio.py
+++ parsers/cardapio.py
@@ -7,13 +7,30 @@ pratos = cardapio.getElementsByTagName('prato')
 
 print('MENU')
 print('-='*30)
+id = 1
 for prato in pratos:
     nome = prato.getElementsByTagName('nome')[0].firstChild.nodeValue
-    id = prato.getAttribute('id')
     print(f"{nome} - {id}")
+    id += 1
 print('-='*30)
-print('')
-escolha = input('Digite o ID do prato para saber mais detalhes: ')
-for prato in pratos:
-    prato_escolhido = prato.getAttribute('id')
-    print(prato_escolhido)
+
+prato_escolhido = int(input('Digite o ID do prato para saber mais detalhes: '))
+print("")
+prato = pratos[prato_escolhido-1]
+
+nome = prato.getElementsByTagName('nome')[0].firstChild.nodeValue
+descricao = prato.getElementsByTagName('descricao')[0].firstChild.nodeValue
+ingredientes = prato.getElementsByTagName('ingredientes')[0].getElementsByTagName('ingrediente')
+preco = prato.getElementsByTagName('preco')[0].firstChild.nodeValue
+moeda = prato.getElementsByTagName('preco')[0].getAttribute('moeda')
+calorias = prato.getElementsByTagName('calorias')[0].firstChild.nodeValue
+tempo_preparo = prato.getElementsByTagName('tempoPreparo')[0].firstChild.nodeValue
+
+print(f"Nome: {nome}")
+print(f"Descrição: {descricao}")
+print("Ingredientes:")
+for ingrediente in ingredientes:
+    print(f" - {ingrediente.firstChild.nodeValue}")
+print(f"Preço: {moeda}{preco}")
+print(f"Calorias: {calorias}")
+print(f"Tempo de Preparo: {tempo_preparo}")
