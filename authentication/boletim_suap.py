3ff528bd86e787b2bfc2a504f6a4d9215dfe8f72
Manoel Cândido <candidomanoel78@gmail.com>
2024-08-30 20:36:43 -0300

    enviando atv boletim suap aula 13

--- authentication/boletim_suap.py
+++ authentication/boletim_suap.py
@@ -2,27 +2,28 @@ import requests
 from getpass import getpass
 
 api_url = "https://suap.ifrn.edu.br/api/"
-athentication_url = f"{api_url}v2/autenticacao/token/"
-boletim_url = f"{api_url}v2/minhas-informacoes/boletim/2024/1/"
-
-user = input("user: ")
+authentication_url = f"{api_url}v2/autenticacao/token/"
+user = input("Usuário: ")
 password = getpass()
 
 data = {"username":user,"password":password}
+response = requests.post(authentication_url, json=data)
+token = response.json()['access']
+headers = {"Authorization": f'Bearer {token}'}
 
-response = requests.post(api_url+"v2/autenticacao/token/", json=data)
-token = response.json()["access"]
-print(response.json())
+ano_letivo = input('Digite o ano letivo: ')
+periodo_letivo = input('Digite o período letivo: ')
+print('')
+boletim_url = f"{api_url}v2/minhas-informacoes/boletim/{ano_letivo}/{periodo_letivo}/"
 
-headers = {
-    "Authorization": f'Bearer {token}'
-}
+response = requests.get(boletim_url, headers=headers)
 
-
-
-skajd = "https://suap.ifrn.edu.br/api/v2/minhas-informacoes/boletim/2024/1/"
-
-response = requests.get(skajd, headers=headers)
-for nome in response.json():
-    print(nome['disciplina'])
-    print(nome['nota_etapa_1']['nota'])
+for data in response.json():
+    print(f"{data['disciplina']}")
+    print(f"Nota etapa 1 - {data['nota_etapa_1']['nota']}")
+    print(f"Nota etapa 2 - {data['nota_etapa_2']['nota']}")
+    print(f"Nota etapa 3 - {data['nota_etapa_3']['nota']}")
+    print(f"Nota etapa 4 - {data['nota_etapa_4']['nota']}")
+    print(f"Média final - {data['media_final_disciplina']}")
+    print(f"Número de faltas - {data['numero_faltas']}")
+    print('')
