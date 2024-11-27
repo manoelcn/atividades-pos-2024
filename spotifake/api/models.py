c3352ea0984d25e71a9d8dc716b0b47713926b7e
Manoel Cândido <candidomanoel78@gmail.com>
2024-11-27 18:18:32 -0300

    add str

--- spotifake/api/models.py
+++ spotifake/api/models.py
@@ -6,14 +6,23 @@ class Artista(models.Model):
     local = models.CharField(max_length=100)
     ano_criacao = models.IntegerField()
 
+    def __str__(self):
+        return self.nome
+
 
 class Album(models.Model):
     artista = models.ForeignKey(Artista, on_delete=models.CASCADE)
     nome = models.CharField(max_length=100)
     ano = models.IntegerField()
 
+    def __str__(self):
+        return self.nome
+
 
 class Musica(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     nome = models.CharField(max_length=100)
     segundos = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
