b2d07868b3b94241c9cea4d0072b1daa9b2881ab
Manoel Cândido <candidomanoel78@gmail.com>
2024-11-27 18:19:28 -0300

    adicionando rotas aninhadas

--- spotifake/api/views.py
+++ spotifake/api/views.py
@@ -3,23 +3,22 @@ from rest_framework import viewsets
 from .models import Artista, Album, Musica
 from .serializers import ArtistaSerializer, AlbumSerializer, MusicaSerializer
 
+
 class ArtistaViewSet(viewsets.ModelViewSet):
-    """
-    Permite a manipulação de dados de Artistas
-    """
     queryset = Artista.objects.all()
     serializer_class = ArtistaSerializer
 
+
 class AlbumViewSet(viewsets.ModelViewSet):
-    """
-    Permite a manipulação de dados de Albuns
-    """
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
 
+    def get_queryset(self):
+        return Album.objects.filter(artista=self.kwargs['artista_pk'])
+
 class MusicaViewSet(viewsets.ModelViewSet):
-    """
-    Permite a manipulação de dados de Músicas
-    """
     queryset = Musica.objects.all()
     serializer_class = MusicaSerializer
+
+    def get_queryset(self):
+        return Musica.objects.filter(album=self.kwargs['album_pk'])
--- spotifake/app/urls.py
+++ spotifake/app/urls.py
@@ -1,18 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-
+from rest_framework import routers  
+from rest_framework_nested import routers
 from api import views
+
 
 router = routers.DefaultRouter()
 router.register(r'artistas', views.ArtistaViewSet)
 router.register(r'albuns', views.AlbumViewSet)
 router.register(r'musicas', views.MusicaViewSet)
 
-# Wire up our API using automatic URL routing.
+artista_router = routers.NestedDefaultRouter(router, r'artistas', lookup='artista')
+artista_router.register(r'albuns', views.AlbumViewSet, basename='artista-albuns')
+
+album_router = routers.NestedDefaultRouter(artista_router, r'albuns', lookup='album')
+album_router.register(r'musicas', views.MusicaViewSet, basename='album-musicas')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-]
+] + artista_router.urls + album_router.urls
 
 urlpatterns += router.urls
