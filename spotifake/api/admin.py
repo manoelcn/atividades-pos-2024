bc6cfb7a831b1e0a80e5298a32ecea7e99281e55
Manoel Cândido <candidomanoel78@gmail.com>
2024-11-27 18:17:50 -0300

    registrando no admin

--- spotifake/api/admin.py
+++ spotifake/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Artista, Album, Musica
 
-# Register your models here.
+
+admin.site.register(Artista)
+admin.site.register(Album)
+admin.site.register(Musica)
