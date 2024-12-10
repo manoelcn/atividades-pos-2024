103e12b4a8a4b543a3b771e28a582b8b2af9d975
manoel <candidomanoel78@gmail.com>
2024-12-10 18:25:32 -0300

    criando componentes

---
+++ clientespotifake/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Footer() {
+    return (
+        <Navbar expand="lg" bg="dark" data-bs-theme="dark" fixed="bottom">
+            <Container className="d-flex justify-content-center">
+                <Navbar.Text className="text-white">SpotiFaique</Navbar.Text>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Footer;
---
+++ clientespotifake/src/components/Menu.jsx
@@ -0,0 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function Menu() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">SpotiFaique</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Artistas</Nav.Link>
+            <Nav.Link href="#home">Álbuns</Nav.Link>
+            <Nav.Link href="#home">Músicas</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Menu;
