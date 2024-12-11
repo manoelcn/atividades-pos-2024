206e6ad879149ec4835320e1f01dc5a96a0164fb
manoel <candidomanoel78@gmail.com>
2024-12-10 23:20:10 -0300

    adicionando rota de musicas

--- clientespotifake/src/App.jsx
+++ clientespotifake/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Index from './pages/Index';
 import Artistas from './pages/Artistas';
 import Albuns from './pages/Albuns';
-// import Musicas from './pages/Musicas';
+import Musicas from './pages/Musicas';
 import Footer from './components/Footer';
 import Menu from './components/Menu';
 
@@ -17,6 +17,7 @@ function App() {
             <Route path="/" element={<Index />} />
             <Route path="/artistas" element={<Artistas />} />
             <Route path="/albuns" element={<Albuns />} />
+            <Route path="/musicas" element={<Musicas />} />
           </Routes>
         </div>
       </Router>
