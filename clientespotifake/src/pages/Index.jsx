0bfaa0ae9e86e7a3c5ac73031cab335784b1433d
Manoel Cândido <candidomanoel78@gmail.com>
2024-12-11 07:28:30 -0300

    removendo imagem

--- clientespotifake/src/pages/Index.jsx
+++ clientespotifake/src/pages/Index.jsx
@@ -7,7 +7,7 @@ const Index = () => {
             <div class="px-4 py-5 my-5 text-center">
                 <div class="col-lg-6 mx-auto">
                     <Card style={{ width: '800px' }} border="0">
-                        <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Spotify_logo_with_text.svg/1200px-Spotify_logo_with_text.svg.png" />
+                        <Card.Img variant="top" src="#" />
                         <Card.Body>
                             <Card.Title className='align-items-center'>Versão totalmente original</Card.Title>
                             <Card.Text>
