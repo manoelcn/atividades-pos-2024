2d0cd9c3475beb78a9e944c2626d489a95fa46ed
manoel <candidomanoel78@gmail.com>
2024-12-10 22:50:03 -0300

    criando pagina de albuns

---
+++ clientespotifake/src/pages/Albuns.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import { getData } from '../services/wrapperAPI';
+
+const Albuns = () => {
+    const [albuns, setAlbuns] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchAlbuns = async () => {
+            try {
+                const data = await getData('albuns');
+                setAlbuns(data);
+            } catch (error) {
+                setError('Erro ao carregar artistas');
+            } finally {
+                setLoading(false);
+            }
+        };
+        
+        fetchAlbuns();
+    }, []);
+
+    if (loading) {
+        return <div>Carregando...</div>;
+    }
+
+    if (error) {
+        return <div>{error}</div>;
+    }
+    
+    return (
+        <div>
+            <h1>Álbuns</h1>
+            <ul>
+                {albuns.map((album) => (
+                    <li key={album.id}>{album.nome}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Albuns;
