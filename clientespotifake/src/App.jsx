918d11a8e520af44dd3d5fee0f435750a34ca31e
manoel <candidomanoel78@gmail.com>
2024-12-10 22:50:29 -0300

    adicionando rota de albuns

--- clientespotifake/src/App.jsx
+++ clientespotifake/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Index from './pages/Index';
 import Artistas from './pages/Artistas';
+import Albuns from './pages/Albuns';
+// import Musicas from './pages/Musicas';
 import Footer from './components/Footer';
 import Menu from './components/Menu';
 
@@ -14,6 +16,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/artistas" element={<Artistas />} />
+            <Route path="/albuns" element={<Albuns />} />
           </Routes>
         </div>
       </Router>
