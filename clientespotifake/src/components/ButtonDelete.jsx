1a90d74f8b01208980b9ba4a067a032d44c470b2
manoel <candidomanoel78@gmail.com>
2024-12-11 22:09:11 -0300

    criando botoes de editar e excluir

---
+++ clientespotifake/src/components/ButtonDelete.jsx
@@ -0,0 +1,9 @@
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+function ButtonDelete() {
+    return (
+        <button className='btn btn-danger'><i class="bi bi-trash" /></button>
+    );
+};
+
+export default ButtonDelete;
---
+++ clientespotifake/src/components/ButtonEdit.jsx
@@ -0,0 +1,9 @@
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+function ButtonEdit() {
+    return (
+        <button className='btn btn-warning'><i class="bi bi-pencil-square" /></button>
+    );
+};
+
+export default ButtonEdit;
